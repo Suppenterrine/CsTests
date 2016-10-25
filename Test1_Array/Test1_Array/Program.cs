be22b93f5c0879247f353e657db3a2d3f9d87597
Lukas Baumert <lukas.baumert@gmx.de>
2016-10-25 16:00:14 +0200

    Es wird geprüft ob die eingegebene Zahl in dem Array vorhanden ist.

---
+++ Test1_Array/Test1_Array/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Test1_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 1, 3, 5, 7, 9 };
+
+            Console.Write("Testen Sie die Daten im Array: ");
+            int iEingabe = Convert.ToInt16(Console.ReadLine());
+
+            /*
+            if(iEingabe = array)
+            {
+                Console.WriteLine("Die Daten sind korrekt.");
+            }
+
+            else
+            {
+                Console.WriteLine("Die Daten sind nicht korrekt.");
+            }
+            */
+
+            Console.ReadKey();
+        }
+    }
+}
